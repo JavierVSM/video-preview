084a1dd53ca629867c252e75b69279c1b5622bff
JavierVSM <javier.vsm@gmail.com>
2021-09-17 16:18:52 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("page loaded...");
+
+
+let vid = document.querySelector('#video'); 
+
+
+function play(element) {
+    vid.play();    
+}
+    
+function pause(element) {
+    vid.pause();   
+}
